dd7f395367ff752c1fc3b6f0ab458b8dac39911a
Harrisonhecker <harrison.hecker22@gmail.com>
2021-11-06 09:57:36 -0400

    Scaffolding for products controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_132042) do
+ActiveRecord::Schema.define(version: 2021_11_06_134322) do
 
   create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
