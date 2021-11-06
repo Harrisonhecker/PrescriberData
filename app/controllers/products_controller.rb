69bef55fdb4a2e1d3ca872c31408d93bbab8b953
Harrisonhecker <harrison.hecker22@gmail.com>
2021-11-06 17:27:00 -0400

    Added code for parsing product information correctly.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,93 @@
 require 'roo' # use this gem for parsing excel files
 
+  def parse_info
+
+    # open the sheet
+    file = Roo::Spreadsheet.open('./Prescriber_Data.csv')
+    sheet = file.sheet(0)
+
+    # iterate over each row and store the correct information, start at row 2 to skip past headers
+    last_row = sheet.last_row
+    row_num = 2
+    productList = [] # array will contain the product objects
+    productNames = [] # array will contain the names of the products
+    while row_num <= last_row do
+        
+        # save data
+        product_id = sheet.cell(row_num, 1) 
+        state = sheet.cell(row_num, 4)
+        product = sheet.cell(row_num, 5)
+        nrx_month_1 = sheet.cell(row_num, 6)
+        nrx_month_2 = sheet.cell(row_num, 7)
+        nrx_month_3 = sheet.cell(row_num, 8)
+        nrx_month_4 = sheet.cell(row_num, 9)
+        nrx_month_5 = sheet.cell(row_num, 10)
+        nrx_month_6 = sheet.cell(row_num, 11)
+        trx_month_1 = sheet.cell(row_num, 12)
+        trx_month_2 = sheet.cell(row_num, 13)
+        trx_month_3 = sheet.cell(row_num, 14)
+        trx_month_4 = sheet.cell(row_num, 15)
+        trx_month_5 = sheet.cell(row_num, 16)
+        trx_month_6 = sheet.cell(row_num, 17)
+        total_nrx = nrx_month_1.to_i + nrx_month_2.to_i + nrx_month_3.to_i + nrx_month_4.to_i + nrx_month_5.to_i + nrx_month_6.to_i
+        total_trx = trx_month_1.to_i + trx_month_2.to_i + trx_month_3.to_i + trx_month_4.to_i + trx_month_5.to_i + trx_month_6.to_i
+
+        # if product does not exist, create it
+        if (productNames.find_index(product).nil?)
+            productNames << product
+            new_product = Product.new(
+            ProductID: product_id,
+            ProductName: product,
+            Month1NRxProduct: nrx_month_1.to_i,
+            Month2NRxProduct: nrx_month_2.to_i,
+            Month3NRxProduct: nrx_month_3.to_i,
+            Month4NRxProduct: nrx_month_4.to_i,
+            Month5NRxProduct: nrx_month_5.to_i,
+            Month6NRxProduct: nrx_month_6.to_i,
+            Month1TRxProduct: trx_month_1.to_i,
+            Month2TRxProduct: trx_month_2.to_i,
+            Month3TRxProduct: trx_month_3.to_i,
+            Month4TRxProduct: trx_month_4.to_i,
+            Month5TRxProduct: trx_month_5.to_i,
+            Month6TRxProduct: trx_month_6.to_i,
+            TotalNRxProduct: total_nrx,
+            TotalTRxProduct: total_trx
+            )
+            productList << new_product
+        # if product does exist, update values
+        else 
+            currentProduct = productList[productNames.find_index(product)]
+            currentProduct.Month1NRxProduct += (nrx_month_1).to_i
+            currentProduct.Month2NRxProduct += (nrx_month_2).to_i
+            currentProduct.Month3NRxProduct += (nrx_month_3).to_i
+            currentProduct.Month4NRxProduct += (nrx_month_4).to_i
+            currentProduct.Month5NRxProduct += (nrx_month_5).to_i
+            currentProduct.Month6NRxProduct += (nrx_month_6).to_i
+            currentProduct.Month1TRxProduct += (trx_month_1).to_i
+            currentProduct.Month2TRxProduct += (trx_month_2).to_i
+            currentProduct.Month3TRxProduct += (trx_month_3).to_i
+            currentProduct.Month4TRxProduct += (trx_month_4).to_i
+            currentProduct.Month5TRxProduct += (trx_month_5).to_i
+            currentProduct.Month6TRxProduct += (trx_month_6).to_i
+            currentProduct.TotalNRxProduct += total_nrx
+            currentProduct.TotalTRxProduct += total_trx
+        end
+        row_num = row_num + 1
+    end
+    Product.import productList
+  end
+
+
+
+
+
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
   
   # GET /products or /products.json
   def index
+    Product.delete_all
+    parse_info
     @products = Product.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :states
   resources :doctors
   resources :products
-  get 'states/index'
-  root to: 'states#index'
+  get 'products/index'
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
